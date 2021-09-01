7d412b74e6ba48169a9e32427f48d940db3d58cc
BoltSheep <thidampe@gmail.com>
2021-09-01 00:16:23 -0300

    Imagen

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,20 +6,18 @@ import Menu from "@material-ui/core/Menu";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import Logar from "../../functions/Logar";
-import SvgIcon from "@material-ui/core/SvgIcon";
 import logo_principal from '../../imagens/Logo_solo.png';
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
-import { ReactComponent as Logo } from "../../imagens/unknown.svg";
+import {ReactComponent as Logo} from "../../imagens/unknown.svg";
 import "./Header.css";
 
 class Header extends React.Component {
